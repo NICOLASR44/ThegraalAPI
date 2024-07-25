8c155a33b07332ea529c3a62de3743ed62651564
NICOLASR44 <158582207+NICOLASR44@users.noreply.github.com>
2024-07-25 21:02:55 +0200

    rout find by id

--- index.js
+++ index.js
@@ -12,8 +12,20 @@ app.get("/lieux", (req, res) => {
   res.json(tabLieux);
 });
 
+app.get("/lieux/:id", (req, res) => {
+  const lieuxId = parseInt(req.params.id);
+  const lieux = tabLieux.find((elem) => elem.id === lieuxId);
+  res.json(lieux);
+});
+
 app.get("/potions", (req, res) => {
   res.json(tabPotions);
+});
+
+app.get("/potion/:id", (req, res) => {
+  const potionId = parseInt(req.params.id);
+  const potion = tabPotions.find((elem) => elem.id === potionId);
+  res.json(potion);
 });
 
 app.listen(serverPort, () => {
